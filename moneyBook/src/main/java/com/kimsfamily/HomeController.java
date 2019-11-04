1b04f9ab36edd73eb780342d4e277cf762f90566
KimJinSu <rnrmfpem@gmail.com>
2019-11-04 19:45:47 +0900

    test c01

--- moneyBook/src/main/java/com/kimsfamily/HomeController.java
+++ moneyBook/src/main/java/com/kimsfamily/HomeController.java
@@ -29,7 +29,9 @@ public class HomeController {
 		new RequestUtil(request, reqmap);
 		
 		ModelAndView mav = new ModelAndView(new StrUtils().replaceLast(request.getRequestURI().toString(), ".do", ""));
+		
 		System.out.println("test git c01");
+		
 		try {
 
 		} catch (Exception e) {
